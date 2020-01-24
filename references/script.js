60842cf6571aa6d646ca6af3d95fb922700f5d31
Wajiha Shah <wshah@oicr.on.ca>
2020-01-24 16:52:56 -0500

    Created a script that ensures other fields are filled if the staging fields have their value set to ajcc.

--- references/script.js
+++ references/script.js
@@ -39,6 +39,39 @@ const scripts = {
             return result;
         }
         validate();
+    },
+    "ajccValidation": () => {
+        function validate() {
+
+            var result = {valid: true, message: "Ok"};
+            
+            /* This is not a required field, so first ensure that it exists */
+            if ($field){
+                /* Contingent on the naming system for tumour staging systems to remain consistent */
+                var stagingName = $name.split('_tumour_staging_system')[0].trim().toLowerCase();
+                /* Perform validation only if an AJCC value was selected */
+                if (/^(AJCC)\b/im.test($field)){
+                    var requiredFields = [
+                        `${stagingName}_m_category`,
+                        `${stagingName}_n_category`,
+                        `${stagingName}_t_category`,
+                        `${stagingName}_stage_group`,
+                        `stage_suffix`
+                    ];
+                    var convertedRow = Object.fromEntries(Object.entries($row).map(([fieldName,fieldVal]) => [fieldName.toLowerCase(), fieldVal]));
+                    /* Check for contigous spaces wrapped with quotes (empty strings) */
+                    var checkforEmpty = (entry) => {return /^\s+$/gm.test(decodeURI(entry).replace(/^"(.*)"$/, '$1'))};
+                    /* search for fields with falsy values, given the field exists */
+                    var emptyFields = requiredFields.filter(field => Object.keys(convertedRow).includes(field) && (!convertedRow[field] || checkforEmpty(convertedRow[field])))
+                    if (emptyFields.length){
+                        result = {valid: false, "message": `The following fields are required when ${$name} is set to an AJCC option: ${emptyFields}`};
+                    }
+                }
+            }
+
+            return result;
+        }
+        validate();
     }
 }
 
