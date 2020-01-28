3ec2ca4a64681158bfcd4c2f697fd12229814c56
wajiha-oicr <59613607+wajiha-oicr@users.noreply.github.com>
2020-01-28 11:07:58 -0500

    Restructure File Hierarchy of Scripts (#40)
     Restructured the script hierarchy. Updating scripts.

--- references/script.js
+++ references/script.js
@@ -1,15 +1,29 @@
-const autoload =require('auto-load');
-const scripts = autoload(__dirname +'/validationFunctions');
+
+const autoload = require('auto-load');
+const files = autoload(__dirname +'/validationFunctions');
 
 const escaper = (funcs) => {
     return String(funcs).replace(/^\(\)[\s]*=>[\s]*{[\s]*/, '').replace(/[\s]*}[\s]*$/,'');
 }
 
-const escapedScripts = Object.fromEntries(
-    Object.entries(scripts).map(
-        ([scriptName,scriptString]) => [scriptName, escaper(scriptString)]
-    )
-);
+const escapeFunctions = (scripts) => {
+    return Object.fromEntries(
+        Object.entries(scripts).map(
+            ([scriptName,scriptString]) => [scriptName, escaper(scriptString)]
+        )
+    );
+}
+
+
+const buildScriptObject = (files) => {
+   return Object.fromEntries(
+        Object.entries(files).map(
+            ([fileName, scriptCollection]) => [fileName, escapeFunctions(scriptCollection)]
+        )
+    );
+}
+
+const escapedScripts = buildScriptObject(files);
 
 module.exports = escapedScripts ;
 
---
+++ references/validationFunctions/sample_registration/specimentypeDesignation.js
@@ -0,0 +1,28 @@
+const validation = () => {
+    function validate() {
+
+        const row = $row;
+        let result = {valid: true, message: "Ok"};
+        
+        const designation = row.tumour_normal_designation.trim().toLowerCase();
+        const specimen_type = $field.trim().toLowerCase();
+        
+        if (designation === "normal"){
+            const validTypes = ["normal", "normal - tissue adjacent to primary tumour", "cell line - derived from normal"];
+            if (!validTypes.includes(specimen_type)){
+                result = {valid: false, message: "Invalid specimen_type. Specimen_type can only be set to a normal type value (Normal, Normal - tissue adjacent to primary tumour, or Cell line - derived from normal) when tumour_normal_designation is set to Normal."};
+            }
+        }
+        else if (designation === "tumour") {
+            const invalidTypes = ["normal", "cell line - derived from normal"];
+            if (invalidTypes.includes(specimen_type)){
+                result = {valid: false, message: "Invalid specimen_type. Specimen_type cannot be set to normal type value (Normal or Cell line - derived from normal) when tumour_normal_designation is set to Tumour."};
+            }
+        }
+        return result;
+    }
+    
+    validate();
+}
+
+module.exports = validation;
