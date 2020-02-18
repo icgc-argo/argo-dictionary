7fe416b2d6aae15dda44856a4892b9f26844b115
Wajiha Shah <wshah@oicr.on.ca>
2020-02-18 16:12:04 -0500

    add ensure deceased to survival time field

--- references/validationFunctions/donor/ensureDeceased.js
+++ references/validationFunctions/donor/ensureDeceased.js
@@ -1,17 +1,17 @@
 /**
  * If and only if a donor is deceased, then the cause of death must be provided.
  */
-const validation = ($row, $field) => 
+const validation = ($row, $field, $name) => 
     (function validate() {
         let result = {valid: true, message: "Ok"};
-        const causeOfDeath = typeof($field) === 'string' ? $field.trim().toLowerCase() : $field;
+        const currField = typeof($field) === 'string' ? $field.trim().toLowerCase() : $field;
         const vitalStatus = $row.vital_status.trim().toLowerCase();
     
-        if (!causeOfDeath && vitalStatus === "deceased"){
-            result = {valid: false, message: "Cause of death must be provided when the donor's vital_status is deceased."}
+        if (!currField && vitalStatus === "deceased"){
+            result = {valid: false, message: `${$name} must be provided when the donor's vital_status is deceased.`}
         }
-        else if (causeOfDeath && vitalStatus != "deceased"){
-            result = {valid: false, message: "A cause of death cannot be provided if the donor's vital_status is not deceased."}
+        else if (currField && vitalStatus != "deceased"){
+            result = {valid: false, message: `${$name} cannot be provided if the donor's vital_status is not deceased.`}
         }
         return result;
     })();
--- tests/donor/ensureDeceased.test.js
+++ tests/donor/ensureDeceased.test.js
@@ -6,73 +6,135 @@ const loadObjects = require('../loadObjects');
 // const donor = require('../dummyData/donor.json');
 const donor = require('../constructDummyData').getSchemaDummy('donor');
 
-// the name of the field being validated
-const name = "cause_of_death";
-
-const unitTests = [
-    [
-        'Deceased Donor without cause of death.',
-        false,
-        loadObjects(donor,
-            {   
-                "vital_status": "deceased"
-            }
-        )
+const myUnitTests = {
+    "cause_of_death": [
+        [
+            'Deceased Donor without cause of death.',
+            false,
+            loadObjects(donor,
+                {   
+                    "vital_status": "deceased"
+                }
+            )
+        ],
+        [
+            'Deceased Donor with cause of death.',
+            true,
+            loadObjects(donor,
+                {   
+                    "vital_status": "deceased",
+                    "cause_of_death" : "Died of Cancer"
+                }
+            )
+            
+        ],
+        [
+            'Living Donor with cause of death.',
+            false,
+            loadObjects(donor,
+                {
+                    "vital_status": "Alive",
+                    "cause_of_death" : "Died of Other Reasons"
+                }
+            )
+        ],
+        [
+            'Living Donor without a cause of death.',
+            true,
+            loadObjects(donor,
+                {
+                    "vital_status": "Alive"
+                }
+            )
+        ],
+        [
+            'Unknown Donor with a cause of death.',
+            false,
+            loadObjects(donor,
+                {   
+                    "vital_status": "unknown",
+                    "cause_of_death": "died of cancer"
+                }
+            )
+        ]
     ],
-    [
-        'Deceased Donor with cause of death.',
-        true,
-        loadObjects(donor,
-            {   
-                "vital_status": "deceased",
-                "cause_of_death" : "Died of Cancer"
-            }
-        )
-        
-    ],
-    [
-        'Living Donor with cause of death.',
-        false,
-        loadObjects(donor,
-            {
-                "vital_status": "Alive",
-                "cause_of_death" : "Died of Other Reasons"
-            }
-        )
-    ],
-    [
-        'Living Donor without a cause of death.',
-        true,
-        loadObjects(donor,
-            {
-                "vital_status": "Alive"
-            }
-        )
-    ],
-    [
-        'Unknown Donor with a cause of death.',
-        false,
-        loadObjects(donor,
-            {   
-                "vital_status": "unknown",
-                "cause_of_death": "died of cancer"
-            }
-        )
+    "survival_time": [
+        [
+            'Deceased Donor without survival time',
+            false,
+            loadObjects(donor,
+                {   
+                    "vital_status": "deceased"
+                }
+            )
+        ],
+        [
+            'Deceased Donor with survival time',
+            true,
+            loadObjects(donor,
+                {   
+                    "vital_status": "deceased",
+                    "survival_time" : "55"
+                }
+            )
+            
+        ],
+        [
+            'Living Donor with survival_time.',
+            false,
+            loadObjects(donor,
+                {
+                    "vital_status": "Alive",
+                    "survival_time" : "65"
+                }
+            )
+        ],
+        [
+            'Living Donor without a survival_time',
+            true,
+            loadObjects(donor,
+                {
+                    "vital_status": "Alive"
+                }
+            )
+        ],
+        [
+            'Unknown Donor with a survival_time.',
+            false,
+            loadObjects(donor,
+                {   
+                    "vital_status": "unknown",
+                    "survival_time": "88"
+                }
+            )
+        ]
     ]
-];
-
+};
 
 describe("Common Tests",()=>{
-    unitTests.forEach(test=>{
-        const testIndex = 2;
-        const testInputs = test[testIndex];
-        universalTest(validation(testInputs,name,testInputs[name]));
+    Object.entries(myUnitTests).forEach(field =>{
+        const name = field[0];
+        const unitTests = field[1];
+        unitTests.forEach(test=>{
+            const testIndex = 2;
+            const testInputs = test[testIndex];
+            universalTest(validation(testInputs,name,testInputs[name]));
+        })
     })
+    
 })
 
-describe("Unit Tests for Ensure Deceased Validation",()=>{
-    test.each(unitTests)('\n Test %# : %s \nExpecting result.valid to be: %s',(description,target,inputs) =>{
-        const scriptOutput = validation(inputs, inputs[name], inputs[inputs.name]);
-        expect(scriptOutput.valid).toBe(target);
+describe("Unit Tests for Ensure Deceased",()=>{
+    Object.entries(myUnitTests).forEach(field => {
+        const name = field[0];
+        const unitTests = field[1];
+        describe(`Tests for the ${name} field.`,()=>{
+            test.each(unitTests)('\n Test %# : %s \nExpecting result.valid to be: %s',(description,target,inputs) =>{
+                const scriptOutput = validation(inputs, inputs[name], name);
+                expect(scriptOutput.valid).toBe(target);
+            })
+        })
+        
     })
+    
 })
