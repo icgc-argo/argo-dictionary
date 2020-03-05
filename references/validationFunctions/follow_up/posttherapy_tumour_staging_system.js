c35d020dcb246504093b3161ea9931448b2cc644
Ann Catton <ann.catton@oicr.on.ca>
2020-03-05 13:46:41 -0500

    🐛 52 - Remove posttherapy_tumour field dependency on disease_status_followup

--- references/validationFunctions/follow_up/posttherapy_tumour_staging_system.js
+++ references/validationFunctions/follow_up/posttherapy_tumour_staging_system.js
@@ -1,9 +1,3 @@
 const ajccValidation = require('../common/ajccValidation');
-const requiredWhenProgression = require('./baseScripts/requiredWhenProgression');
-const requiredWhenRelapse = require('./baseScripts/requiredWhenRelapse');
 
-module.exports = [
-    ajccValidation,
-    requiredWhenProgression,
-    requiredWhenRelapse
-];
+module.exports = [ajccValidation];
