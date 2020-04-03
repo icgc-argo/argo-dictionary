75504f58276b23eeeea41230d8c1f30ce3ad9478
May Terry <MayT@mitre.org>
2020-04-03 12:49:31 -0400

    First pass of c19_condition

--- schemas/index.js
+++ schemas/index.js
@@ -1,7 +1,7 @@
 const sample_registration = require('./sample_registration.json');
 const donor = require('./donor.json');
 const specimen = require('./specimen.json');
-const primary_diagnosis = require('./primary_diagnosis.json');
+const c19_condition = require('./c19_condition.json');
 const treatment = require('./treatment.json');
 const chemotherapy = require('./chemotherapy.json');
 const hormone_therapy = require('./hormone_therapy.json');
@@ -12,7 +12,7 @@ module.exports = [
   sample_registration,
   donor,
   specimen,
-  primary_diagnosis,
+  c19_condition,
   treatment,
   chemotherapy,
   hormone_therapy,
