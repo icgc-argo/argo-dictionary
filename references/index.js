bc61d426d3082087997cc5d1c1d433ec2b8cc450
Rosi <rosi.bajari@gmail.com>
2020-06-02 13:59:48 -0400

    first notes reference example

--- references/index.js
+++ references/index.js
@@ -1,5 +1,6 @@
 const script = require('./script.js');
 const list = require('./list.json');
 const regex = require('./regex.json');
+const notes = require('./notes.json');
 
-module.exports = { script, list, regex };
+module.exports = { script, list, regex, notes };
