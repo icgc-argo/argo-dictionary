f30ca56929b4edb4c113b94ba270dc9b1578d3f0
Hardeep <hardeep.nahal@oicr.on.ca>
2020-06-03 16:22:59 -0400

    updated other conditional fields to use requiredWhenProgressOrRelapseRecur validation script

--- tests/follow_up/requiredWhenProgressOrRelapseRecur.test.js
+++ tests/follow_up/requiredWhenProgressOrRelapseRecur.test.js
@@ -9,6 +9,87 @@ const followUp = require('../constructDummyData').getSchemaDummy('follow_up');
 
 // key -> name of field, value -> unit tests
 const myUnitTests = {
+    'relapse_interval': [
+        [
+            'Disease status is relapse, with provided relapse interval',
+            true,
+            loadObjects(followUp,
+                {   
+                    "disease_status_at_followup": "relapse or recurrence",
+                    "relapse_interval": "500"
+                }
+            )
+        ],
+        [
+            'Disease status is not relapse, with provided relapse interval',
+            false,
+            loadObjects(followUp,
+                {   
+                    "disease_status_at_followup": "partial remission",
+                    "relapse_interval": "900"
+                }
+            )
+        ],
+        [
+            'Disease status is relapse, without provided relapse interval',
+            false,
+            loadObjects(followUp,
+                {   
+                    "disease_status_at_followup": "relapse or recurrence"
+                }
+            )
+        ],
+        [
+            'Disease status is relapse, but relapse interval is just whitespace',
+            false,
+            loadObjects(followUp,
+                {   
+                    "disease_status_at_followup": "relapse or recurrence",
+                    "relapse_interval" : "      "
+                }
+            )
+        ],
+        [
+            'Disease status is not relapse or recurrence, and relapse interval is not provided',
+            true,
+            loadObjects(followUp,
+                {   
+                    "disease_status_at_followup": "stable"
+                }
+            )
+        ],
+    ],
+    'relapse_type': [
+        [
+            'Disease status is relapse, and relapse type is Local recurrence',
+            true,
+            loadObjects(followUp,
+                {   
+                    "disease_status_at_followup": "relapse or recurrence",
+                    "relapse_type": "local recurrence"
+                }
+            )
+        ],
+        [
+            'Disease status is stable, and relapse type is recurrence',
+            false,
+            loadObjects(followUp,
+                {   
+                    "disease_status_at_followup": "stable",
+                    "relapse_type": "Distant recurrence/metastasis"
+                }
+            )
+        ],
+        [
+            'Disease status is complete remission, and relapse type is not provided',
+            true,
+            loadObjects(followUp,
+                {   
+                    "disease_status_at_followup": "complete remission",
+                }
+            )
+        ],
+    ],
     'anatomic_site_progression_or_recurrences': [
         [
             'Disease status is distant progression, with provided ASPOR',
@@ -67,9 +148,128 @@ const myUnitTests = {
                 }
             )
         ]
-   ]
-}
+   ],
+    'recurrence_tumour_staging_system': [
+        [
+            'Disease status is relapse, with provided RTSS',
+            true,
+            loadObjects(followUp,
+                {   
+                    "disease_status_at_followup": "relapse or recurrence",
+                    "recurrence_tumour_staging_system": "AJCC 8th edition"
+                }
+            )
+        ],
+        [
+            'Disease status is relapse, without provided RTSS',
+            false,
+            loadObjects(followUp,
+                {   
+                    "disease_status_at_followup": "relapse or recurrence"
+                }
+            )
+        ],
+        [
+            'Disease status is distant progression, without provided RTSS',
+            false,
+            loadObjects(followUp,
+                {   
+                    "disease_status_at_followup": "distant progression"
+                }
+            )
+        ],
+        [
+            'Disease status is loco-regional progression, without provided RTSS',
+            true,
+            loadObjects(followUp,
+                {   
+                    "disease_status_at_followup": "loco-regional progression",
+                    "recurrence_tumour_staging_system": "Binet staging system"
+                    
+                }
+            )
+        ],
+        [
+            'Disease status is no evidence of disease, without provided RTSS',
+            true,
+            loadObjects(followUp,
+                {   
+                    "disease_status_at_followup": "no evidence of disease"
+                }
+            )
+        ],
+        [
+            'Disease status is no evidence of disease, without provided RTSS',
+            false,
+            loadObjects(followUp,
+                {   
+                    "disease_status_at_followup": "no evidence of disease",
+                    "recurrence_tumour_staging_system": "Binet staging system"
+                }
+            )
+        ]
+    ],
+    'method_of_progression_status': [
 
+        [
+            'Disease status is distant progression, with provided method of progression',
+            true,
+            loadObjects(followUp,
+                {   
+                    "disease_status_at_followup": "Distant progression",
+                    "method_of_progression_status": "Biopsy"
+                }
+            )
+        ],
+        [
+            'Disease status is Loco-regional progression, with provided method of progression',
+            true,
+            loadObjects(followUp,
+                {   
+                    "disease_status_at_followup": "Loco-regional progression",
+                    "method_of_progression_status": "blood draw"
+                }
+            )
+        ],
+        [
+            'Disease status is distant progression, without method of progression',
+            false,
+            loadObjects(followUp,
+                {   
+                    "disease_status_at_followup": "Distant progression"
+                }
+            )
+        ],
+        [
+            'Disease status is Loco-regional progression, without method of progression',
+            false,
+            loadObjects(followUp,
+                {   
+                    "disease_status_at_followup": "Loco-regional progression"
+                }
+            )
+        ],
+        [
+            'Disease status is partial remission, without method of progression',
+            true,
+            loadObjects(followUp,
+                {   
+                    "disease_status_at_followup": "partial remission"
+                }
+            )
+        ],
+        [
+            'Disease status is partial remission, with method of progression',
+            false,
+            loadObjects(followUp,
+                {   
+                    "disease_status_at_followup": "partial remission",
+                    "method_of_progression_status": "imaging"
+                }
+            )
+        ]
+    ]
+}
 
 describe("Common Tests",()=>{
     Object.entries(myUnitTests).forEach(field =>{
