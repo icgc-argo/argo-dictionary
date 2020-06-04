5730e6e2b84d405dc42fc34c57c71c2c7182a040
Rosi Bajari <rosita.bajari@oicr.on.ca>
2020-06-04 11:02:10 -0400

    0528 update reference files (#158)
    * staging system updated
    
    * add missing validation_dependency meta
    
    * follow-up file edits
    
    * follloup --> followup typo fix
    
    * alphabetize sample_registration
    
    * updates to dictionary 49
    
    * remove invalid normal validation data
    
    * clinical trial updates
    
    * progression
    
    * relpase/recurrence updates
    
    * update grade
    
    * update files with optional treatment/donor fields
    
    * small description update - registration file
    
    * standardize file description for multiple input instructions
    
    * standardize file schema descriptions

--- references/validationFunctions/follow_up/baseScripts/requiredWhenRelapse.js
+++ references/validationFunctions/follow_up/baseScripts/requiredWhenRelapse.js
@@ -1,28 +1,35 @@
 /**
  * Enforces a required field if disease status at followup is progression or relapse
- * @param {object} $row 
- * @param {string} $field 
- * @param {string} $name 
+ * @param {object} $row
+ * @param {string} $field
+ * @param {string} $name
  */
-const validation = ($row, $field, $name) => 
-    (function validate() {
-        let result = {valid: true, message: "Ok"};
-        
-        /* required field, cannot be null */
-        const diseaseStatus = $row.disease_status_at_followup.trim().toLowerCase();
+const validation = ($row, $field, $name) =>
+  (function validate() {
+    let result = { valid: true, message: 'Ok' };
 
-        const isRequired = diseaseStatus === "relapse or recurrence";
+    /* required field, cannot be null */
+    const diseaseStatus = $row.disease_status_at_followup.trim().toLowerCase();
 
-        // checks for a string just consisting of whitespace
-        const checkforEmpty = (entry) => {return /^\s+$/g.test(decodeURI(entry).replace(/^"(.*)"$/, '$1'))};
+    const isRequired = diseaseStatus === 'relapse or recurrence';
 
-        if (isRequired && (!$field || checkforEmpty($field))){
-            result = {valid: false, message: `'${$name}' is a required field if 'disease_status_at_followup' is set to 'relapse or recurrence'.` }
-        }
-        else if (!isRequired && ! (!$field || checkforEmpty($field))){
-            result = {valid: false, message: `'${$name}' cannot be provided if 'disease_status_at_folloup' is not set to 'relapse or recurrence'.` }
-        }
-        return result;
-    })();
+    // checks for a string just consisting of whitespace
+    const checkforEmpty = entry => {
+      return /^\s+$/g.test(decodeURI(entry).replace(/^"(.*)"$/, '$1'));
+    };
+
+    if (isRequired && (!$field || checkforEmpty($field))) {
+      result = {
+        valid: false,
+        message: `'${$name}' is a required field if 'disease_status_at_followup' is set to 'relapse or recurrence'.`,
+      };
+    } else if (!isRequired && !(!$field || checkforEmpty($field))) {
+      result = {
+        valid: false,
+        message: `'${$name}' cannot be provided if 'disease_status_at_followup' is not set to 'relapse or recurrence'.`,
+      };
+    }
+    return result;
+  })();
 
 module.exports = validation;
