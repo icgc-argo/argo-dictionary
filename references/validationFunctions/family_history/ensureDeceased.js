4bc69ad35f7c5d9f89599458995684419e9c6c64
Hardeep <hardeep.nahal@oicr.on.ca>
2021-04-21 12:53:49 -0400

    Added validation script to ensure cancer-related fields are not submitted if relative has no or unknown history of cancer, and another validation script to check that the cause of death is consistent with the relative's vital status. Also added new optional field to capture relative's vital status since they were diagnosed with cancer

---
+++ references/validationFunctions/family_history/ensureDeceased.js
@@ -0,0 +1,37 @@
+/*
+ * Copyright (c) 2020 The Ontario Institute for Cancer Research. All rights reserved
+ *
+ * This program and the accompanying materials are made available under the terms of the GNU Affero General Public License v3.0.
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY                           
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES                          
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT                           
+ * SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,                                
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED                          
+ * TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;                               
+ * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER                              
+ * IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN                         
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *  
+ *
+ */
+
+/**
+ * The relative's cause of death should only be provided if the relative is deceased.
+ */
+const validation = () => 
+    (function validate(inputs) {
+        const {$row, $name, $field} = inputs;
+        let result = {valid: true, message: "Ok"};
+        const vitalStatus = $row.relative_vital_status.trim().toLowerCase();
+    
+        if ($field && vitalStatus != "deceased"){
+            result = {valid: false, message: `${$name} cannot be provided if the relative's vital_status is not deceased.`}
+        }
+        return result;
+    });
+
+
+module.exports = validation;
