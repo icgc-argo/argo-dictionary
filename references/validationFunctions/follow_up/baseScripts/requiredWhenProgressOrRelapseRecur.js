72d599cf3b7f1d5d80d0d1f6000723b37453b7a9
Hardeep <hardeep.nahal@oicr.on.ca>
2021-04-27 15:01:41 -0400

    Updated follow up validation scripts to validate correctly when disease_status_at_followup is null. Also added range restriction values to prevent submission of values less than 0.

--- references/validationFunctions/follow_up/baseScripts/requiredWhenProgressOrRelapseRecur.js
+++ references/validationFunctions/follow_up/baseScripts/requiredWhenProgressOrRelapseRecur.js
@@ -31,22 +31,22 @@ const validation = () =>
         let result = {valid: true, message: "Ok"};
         
         /* required field, cannot be null */
-        const diseaseStatus = $row.disease_status_at_followup.trim().toLowerCase();
-    
-        const stateOfProgression = (entry) => {return /(progression)$/.test(decodeURI(entry))}; 
-        const relapseOrRecurrence = diseaseStatus === "relapse or recurrence";
+        if ($row.disease_status_at_followup != null) {
+           const diseaseStatus = $row.disease_status_at_followup.trim().toLowerCase();
+           const stateOfProgression = (entry) => {return /(progression)$/.test(decodeURI(entry))}; 
+           const relapseOrRecurrence = diseaseStatus === "relapse or recurrence";
    
-        // checks for a string just consisting of whitespace
-        const checkforEmpty = (entry) => {return /^\s+$/g.test(decodeURI(entry).replace(/^"(.*)"$/, '$1'))};
+           /* checks for a string just consisting of whitespace */
+           const checkforEmpty = (entry) => {return /^\s+$/g.test(decodeURI(entry).replace(/^"(.*)"$/, '$1'))};
 
-
-        if ((!$field || checkforEmpty($field)) && (stateOfProgression(diseaseStatus) || relapseOrRecurrence)) {
-            result = {valid: false, message: `'${$name}' is a required field if 'disease_status_at_followup' is set a state of progression, relapse, or recurrence.` }
-        }
-        else if (!(!$field || checkforEmpty($field)) && !stateOfProgression(diseaseStatus) && !relapseOrRecurrence) {
-            result = {valid: false, message: `'${$name}' cannot be provided if 'disease_status_at_followup' is not a state of progression, relapse, or recurrence.` }
+           if ((!$field || checkforEmpty($field)) && (stateOfProgression(diseaseStatus) || relapseOrRecurrence)) {
+              result = {valid: false, message: `'${$name}' is a required field if 'disease_status_at_followup' is set a state of progression, relapse, or recurrence.` }
+           }
+           else if (!(!$field || checkforEmpty($field)) && !stateOfProgression(diseaseStatus) && !relapseOrRecurrence) {
+              result = {valid: false, message: `'${$name}' cannot be provided if 'disease_status_at_followup' is not a state of progression, relapse, or recurrence.` }
+           }
         }
         return  result;
-    });
+  });
 
 module.exports = validation;
